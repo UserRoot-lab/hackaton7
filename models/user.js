165bdbd0a9b7aafabcec04390d1818e717fd3d28
Hector <userlinux1122@gmail.com>
2021-03-22 14:00:58 +0100

    Cambio Modelo
    Cambio User

--- models/user.js
+++ models/user.js
@@ -3,11 +3,12 @@ const { Schema } = mongoose
 
 
 const schemaUser = new Schema({
-    nombre: { type: String, required: true },
-    apellido: { type: String, required: true },
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
     email: { type: String, required: true, index: true, unique: true },
     password: { type: String, required: false },
     admin: { type: Boolean, default: true },
+    points: {type: Number , default: 0 }
 });
 
 module.exports = mongoose.model('user', schemaUser)
